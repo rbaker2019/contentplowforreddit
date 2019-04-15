5fb93669121d9ec01a7af1bfff4f18731987ea8f
N/A <N/A>
2019-04-15 18:22:10 -0500

    update menu items and move logic to clickhandler

--- src/background.js
+++ src/background.js
@@ -2,41 +2,38 @@
 
 browser.contextMenus.create({
     id: "submit-url-reddit",
-    title: "Submit this page on Reddit",
+    title: "Link to page on Reddit",
     contexts: ["page", "tab"]
 });
 
 browser.contextMenus.create({
     id: "submit-link-reddit",
-    title: "Submit this link on Reddit",
+    title: "Submit link on Reddit",
     contexts: ["link"]
 });
 
 browser.contextMenus.create({
-    id: "submit-image-reddit",
-    title: "Submit this image on Reddit",
+    id: "submit-image-link-reddit",
+    title: "Link to image on Reddit",
     contexts: ["image"]
 });
 
 browser.contextMenus.create({
-    id: "submit-video-reddit",
-    title: "Submit this video on Reddit",
+    id: "submit-video-link-reddit",
+    title: "Link to video on Reddit",
     contexts: ["video"]
 });
 
 browser.contextMenus.create({
     id: "submit-text-title-reddit",
-    title: "Submit this text as title on Reddit",
+    title: "Use selection as title on Reddit",
     contexts: ["selection"]
 });
 
 browser.contextMenus.create({
     id: "submit-text-body-reddit",
-    title: "Submit this text as self post on Reddit",
+    title: "Use selection as self post on Reddit",
     contexts: ["selection"]
 });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    console.dir(info);
-    console.dir(tab);
-});
+browser.contextMenus.onClicked.addListener(clickHandler);
